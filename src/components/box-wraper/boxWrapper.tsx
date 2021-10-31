19ffd2e1ab0337d12eba8574bf84f11fe4efc708
dolphin <dolphin@dolphins-Mac-mini.local>
2021-10-31 12:41:58 +0330

    item label edit

--- src/components/box-wraper/boxWrapper.tsx
+++ src/components/box-wraper/boxWrapper.tsx
@@ -17,26 +17,12 @@ export const BoxWrapper: React.FunctionComponent<BoxWrapperProps> = ({
   return (
     <View style={styles.container}>
       {list.map((item, key) => {
-        // for (var i = 0; i <=item.count; i++) {
           return (
             <BoxItem type={item.value} key = {key}>
-              <Text>{`${key}`}</Text>
+              <Text>Slot-{key}</Text>
             </BoxItem>
           );
-        // }
       })}
-      {/* <BoxItem type='xl'>
-                <Text>spot1</Text>
-            </BoxItem>
-            <BoxItem type='l'>
-                <Text>spot1</Text>
-            </BoxItem>
-            <BoxItem type='sm'>
-                <Text>spot1</Text>
-            </BoxItem>
-            <BoxItem type='sm'>
-                <Text>spot1</Text>
-            </BoxItem> */}
     </View>
   );
 };
